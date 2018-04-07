bef26c27a2d11433efa7788c29d72168d9ee3a08
wangweiwei <will_workhard@163.com>
2018-04-07 09:09:13 +0800

    创建vuex

---
+++ admin/src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import app from './modules/app'
+import getters from './getters'
+import errorLog from './modules/errorLog'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  modules: {
+    app,
+    errorLog
+  },
+  getters
+})
+
+export default store
