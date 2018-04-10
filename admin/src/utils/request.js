91a96be9400846100a07b6886d6b631380950092
wangweiwei <will_workhard@163.com>
2018-04-10 13:10:03 +0800

    update complete request

--- admin/src/utils/request.js
+++ admin/src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
-import { store } from '@/store'
+import store from '@/store'
 import { getToken } from './auth'
 
 const service = axios.create({
@@ -8,7 +8,31 @@ const service = axios.create({
   timeout: 5000
 })
 
-service.interceptors.request.use(config => {
-  // 请求发出前的拦截设置
-  
-})
+service.interceptors.request.use(
+  config => {
+    // 请求发出前的拦截设置
+    if (store.getters.token) {
+      config.headers['X-Token'] = getToken()
+    }
+    return config
+  },
+  error => {
+    console.log(error)
+    Promise.reject(error)
+  }
+)
+
+service.interceptors.response.use(
+  response => response,
+  error => {
+    console.log('err: ' + error)
+    Message({
+      type: 'error',
+      message: error.message,
+      duration: 5 * 1000
+    })
+    return Promise.reject(error)
+  }
+)
+
+export default service
