3a9d0760aa6b5daef65255ffd5e900efc64d4d54
wangweiwei <will_workhard@163.com>
2018-04-10 18:49:54 +0800

    添加 404、401页面

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -4,9 +4,13 @@ const _import = require('./_import_' + process.env.NODE_ENV)
 
 Vue.use(Router)
 
+export const constantRouterMap = [
+  { path: '/login', name: 'login', component: _import('login/index') },
+  { path: '/', component: _import('test/index') },
+  { path: '/404', component: _import('errorPages/404'), hidden: true }
+]
+
 export default new Router({
-  routes: [
-    { path: '/', component: _import('test/index') },
-    { path: '/login', name: 'login', component: _import('login/index') }
-  ]
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
 })
