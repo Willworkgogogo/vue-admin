26d557a545c6b93fdbd357ad3bcd603732a71037
wangweiwei <will_workhard@163.com>
2018-04-11 22:17:43 +0800

    add 改变用户角色store

--- admin/src/store/modules/app.js
+++ admin/src/store/modules/app.js
@@ -1,18 +1,31 @@
 import Cookies from 'js-cookie'
+import * as CONFIG from '@/api/config'
 
 const app = {
   state: {
-    language: Cookies.get('language') || 'en'
+    language: Cookies.get('language') || 'en',
+    sidebar: {
+      opened: Boolean(+Cookies.get('sidebarStatus'))
+    }
   },
   mutations: {
     SET_LANGUAGE: (state, language) => {
       state.language = language
       Cookies.set('language', language)
+    },
+    TOGGLE_SIDEBAR: state => {
+      state.sidebar.opened
+        ? Cookies.set('sidebarStatus', CONFIG.SIDEBAR_CLOSE)
+        : Cookies.set('sidebarStatus', CONFIG.SIDEBAR_OPEN)
+      state.sidebar.opened = !state.sidebar.opened
     }
   },
   actions: {
     setLanguage({ commit }, language) {
       commit('SET_LANGUAGE', language)
+    },
+    toggleSideBar({ commit }) {
+      commit('TOGGLE_SIDEBAR')
     }
   }
 }
