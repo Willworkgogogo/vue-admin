a793d1cce56b4e10810c1447e4bd1eef7f9c70e5
wangweiwei <will_workhard@163.com>
2018-04-11 22:19:38 +0800

    layout component

--- admin/src/views/layout/Layout.vue
+++ admin/src/views/layout/Layout.vue
@@ -1,14 +1,28 @@
 <template>
-  <div class="app-wrapper">
-
+  <div class="app-wrapper" :class="{hideSidebar: !sidebar.opened}">
+    <sidebar class="app-wrapper"></sidebar>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-export default {}
+import { Sidebar } from './components'
+export default {
+  components: { Sidebar },
+  computed: {
+    sidebar() {
+      console.log('sidebar: ', this.$store.state.app.sidebar)
+      return this.$store.state.app.sidebar
+    }
+  }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~@/styles/mixin"
 
+  .app-wrapper
+    @include clearfix
+    position relative
+    height 100%
+    width 100%
 </style>
