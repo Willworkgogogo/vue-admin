0546be8e4399239dbf31bce523446a5faff6fb44
wangweiwei <will_workhard@163.com>
2018-05-24 10:26:04 +0800

    update

--- admin/src/views/layout/components/Sidebar/index.vue
+++ admin/src/views/layout/components/Sidebar/index.vue
@@ -1,6 +1,6 @@
 <template>
   <scroll-bar>
-    <el-menu mode="vertical" :default-active="$route.path" :collapse="isCollapse" background-color="#304156" text-color="#bfcbd9" active-text-color="#409EFF">
+    <el-menu mode="vertical" :collapse-transition="false" :show-timeout="200" :default-active="$route.path" :collapse="isCollapse" background-color="#304156" text-color="#bfcbd9" active-text-color="#409EFF">
       <sidebar-item :routes="permission_routers"></sidebar-item>
     </el-menu>
   </scroll-bar>
@@ -15,7 +15,7 @@ export default {
   components: { SidebarItem, ScrollBar },
   computed: {
     ...mapGetters(['permission_routers', 'sidebar']),
-    isCollapse() {
+    isCollapse () {
       console.log(this.permission_routers)
       return this.sidebar.opened
     }
